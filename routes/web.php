1d3db43668e031902ccf67abf4a25efb6adcd74a
jmartinez10759 <jorge.martinez.developer@gmail.com>
2018-03-29 17:54:25 -0600

    Se registro el Login para realizar la parte de candidatos en el CV.

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,38 @@ Route::get('/', function () {
     return view('index');
     #return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*Llamadas al controlador Candidate*/
+/*Route::get('login', 'Candidatos\CandidatosController@get_login')->name('login'); // Mostrar login
+Route::post('login', 'Candidatos\CandidatosController@store')->name('login'); // Verificar datos
+#Route::get('logout', 'AuthController@logOut'); // Finalizar sesión
+
+//Rutas privadas solo para usuarios autenticados
+Route::group(['before' => 'auth'], function(){
+
+    Route::get('/home', [
+    	'uses' 		=> 'HomeController@index'
+    	,'as' 		=> 'home'
+	]);
+
+});*/
+
+################################# Se crea un grupo de rutas que se necesita estar loggeado ######################################
+/*Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('home', [
+    	'uses' 		=> 'HomeController@index'
+    	,'as' 		=> 'home'
+	]);
+
+
+
+});*/
+
+###################################### GRUPO DE RUTAS SIN AUTH ############################
 
 Route::get('/index', 'ListadoController@index')->name('carga');
 Route::get('/listado', 'ListadoController@listado')->name('get_list');
@@ -26,3 +54,7 @@ Route::get('/listado', 'ListadoController@listado')->name('get_list');
 ###########################  Registro de Candidatos ##############################
 Route::post('register/insert', 'Candidatos\CandidatosController@create')->name('create');
 Route::post('candidate/login', 'Candidatos\CandidatosController@store')->name('store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
