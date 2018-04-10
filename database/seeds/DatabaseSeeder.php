9bfa535573bdd1f7db4de95d297db354a6fe88d6
jmartinez10759 <jorge.martinez.developer@gmail.com>
2018-04-10 18:34:55 -0500

    creacio de seeder de la tabla ofertas para que se vean reflejados los cambios en master

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+         $this->call(BlmJobsOffersSeeder::class);
          $this->call(BlmNivelesAcademicosSeeder::class);
          $this->call(BlmCategoriasSeeder::class);
     }
